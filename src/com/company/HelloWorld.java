929c7a247ceea6a4607530e52f806c07cd59acb2
vijayan2207 <vijayan.selvaraj@gmail.com>
2020-11-03 11:21:32 +0530

    Added code for multiples of 5

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -3,11 +3,16 @@ package com.company;
 public class HelloWorld {
     public static void main(String args[])
     {
-        System.out.println("\nFind the numbers that are multiples of 3 between 1 and 200: ");
+        System.out.println("\nFind the numbers that are multiples of 3 & 5 between 1 and 100: ");
 
-        for (int i=1; i<200; i++) {
-            if (i%5==0)
+        for (int i=1; i<100; i++) {
+            if (i%3==0)
                 System.out.print(i +", ");
+        }
+
+        for (int j=1; j<100; j++) {
+            if (j%5==0)
+                System.out.print(j +", ");
         }
     }
 }
